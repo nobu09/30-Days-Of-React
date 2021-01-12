9bb905a45a299c6fe92d9c95a1442a55cd489634
nobu09 <nobu0922@gmail.com>
2021-01-13 08:48:57 +0900

    Day1 3 Array Exercise begin

---
+++ solutions/day-01/3_level1.js
@@ -0,0 +1,29 @@
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya',
+]
+
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB',
+]
+
+const arr_empty = []
+
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.length)
