38b1438474e9221e51333da3aba33fe819149da9
nobu09 <nobu0922@gmail.com>
2021-01-15 08:47:47 +0900

    Day1 3 Array Exercise

--- solutions/day-01/3_level1.js
+++ solutions/day-01/3_level1.js
@@ -27,3 +27,30 @@ const arr_empty = []
 const numbers = [1, 2, 3, 4, 5]
 
 console.log(numbers.length)
+
+console.log(numbers[0], numbers[2], numbers[4])
+
+const mixedDataTypes = [
+  1,
+  'string',
+  true,
+  false,
+  { key: 'object' },
+]
+
+console.log(mixedDataTypes)
+
+const itCompanies = [
+  'Facebook',
+  'Google',
+  'Microsoft',
+  'Apple',
+  'IBM',
+  'Oracle',
+  'Amazon',
+]
+
+console.log(itCompanies)
+console.log(itCompanies.length)
+
+
