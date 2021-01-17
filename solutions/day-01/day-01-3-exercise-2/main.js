08d50adf09d8d393fd31441e5f627bb7264259dd
nobu09 <nobu0922@gmail.com>
2021-01-18 08:34:58 +0900

    Day1 3 Array Exercise level2 続き

--- solutions/day-01/day-01-3-exercise-2/main.js
+++ solutions/day-01/day-01-3-exercise-2/main.js
@@ -1,2 +1,20 @@
 console.log(countries);
 console.log(webTechs);
+
+let text =
+  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+
+const textArray = text.split(/[\s,.]+/);
+console.log(textArray);
+console.log(textArray.length);
+
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+if (!shoppingCart.includes('Meat')) {
+  shoppingCart.unshift('Meat');
+}
+console.log(shoppingCart);
+
+if (!shoppingCart.includes('Sugar')) {
+  shoppingCart.push('Sugar');
+}
+console.log(shoppingCart);
