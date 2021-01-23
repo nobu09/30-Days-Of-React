3489c7dfc89fcf5fc02591acaa3936ea4b803993
nobu09 <nobu0922@gmail.com>
2021-01-23 18:14:05 +0900

    Day1 3 Array Exercise level3 はじめ

---
+++ solutions/day-01/day-01-3-exercise-3/main.js
@@ -0,0 +1,6 @@
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
+
+console.log(ages.sort());
+
+console.log(`minimum of ages: ${Math.min(...ages)}`)
+console.log(`maximum of ages: ${Math.max(...ages)}`)
