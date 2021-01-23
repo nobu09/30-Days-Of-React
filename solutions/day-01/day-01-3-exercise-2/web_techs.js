441216bb7603741e4ac8b233b8940de068dabcd3
nobu09 <nobu0922@gmail.com>
2021-01-23 18:12:11 +0900

    Day1 3 Array Exercise level2 終わり

--- solutions/day-01/day-01-3-exercise-2/web_techs.js
+++ solutions/day-01/day-01-3-exercise-2/web_techs.js
@@ -5,3 +5,9 @@ const webTechs = [
   "JavaScript",
   "Sass",
 ]
+
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node', 'Express', 'MongoDB']
+const fullStack = frontEnd.concat(backEnd);
+
+console.log(fullStack)
