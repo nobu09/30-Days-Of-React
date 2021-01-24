40a9613cfa03cd33000e863e4a712f802459868b
nobu09 <nobu0922@gmail.com>
2021-01-24 09:28:57 +0900

    Day1 3 Array Exercise level3 中央値を求める

--- solutions/day-01/day-01-3-exercise-3/main.js
+++ solutions/day-01/day-01-3-exercise-3/main.js
@@ -1,6 +1,17 @@
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 
-console.log(ages.sort());
+const sortedAges = ages.sort();
+console.log(sortedAges);
 
 console.log(`minimum of ages: ${Math.min(...ages)}`)
 console.log(`maximum of ages: ${Math.max(...ages)}`)
+
+const length = sortedAges.length;
+let median = 0;
+if (length % 2 === 0) {
+  median = (sortedAges[length / 2 - 1] + sortedAges[length / 2]) / 2;
+} else {
+  median = sortedAges[length / 2];
+}
+
+console.log(`median of ages: ${median}`)
