a0b22bcb6629c8c1cc085dbc3bbe23140fb6569f
nobu09 <nobu0922@gmail.com>
2021-02-01 09:30:30 +0900

    Day1 4 Conditionals Exercise level1 compare ages

--- solutions/day-01/day-01-4-exercise-1/index.js
+++ solutions/day-01/day-01-4-exercise-1/index.js
@@ -1,12 +1,29 @@
-let age = prompt('Enter your age:')
+let age = prompt("Enter your age:");
 
 switch (true) {
   case Number.isNaN(Number(age)):
-    console.log('You input was not number.')
-    break
+    console.log("You input was not number.");
+    break;
   case age >= 18:
-    console.log('You are old enough to drive.')
-    break
+    console.log("You are old enough to drive.");
+    break;
   default:
-    console.log(`You are left with ${18-age} years to drive.`)
+    console.log(`You are left with ${18 - age} years to drive.`);
+}
+
+let yourAge = prompt("Enter your age:");
+
+switch (true) {
+  case Number.isNaN(Number(yourAge)):
+    console.log("You input was not number.");
+    break;
+  case yourAge > 41:
+    console.log(`You are ${yourAge - 41} years older than me.`);
+    break;
+  case yourAge == 41:
+    console.log("You are same ages to me.");
+    break;
+  case yourAge < 41:
+    console.log(`You are ${41 - yourAge} years younger than me.`);
+    break;
 }
