df63035371d0e360ea0cc32768ed30a71a787008
nobu09 <nobu0922@gmail.com>
2021-02-02 08:44:19 +0900

    Day1 4 Conditionals Exercise level1 compare number

--- solutions/day-01/day-01-4-exercise-1/index.js
+++ solutions/day-01/day-01-4-exercise-1/index.js
@@ -1,29 +1,42 @@
-let age = prompt("Enter your age:");
+// let age = prompt("Enter your age:");
 
-switch (true) {
-  case Number.isNaN(Number(age)):
-    console.log("You input was not number.");
-    break;
-  case age >= 18:
-    console.log("You are old enough to drive.");
-    break;
-  default:
-    console.log(`You are left with ${18 - age} years to drive.`);
+// switch (true) {
+//   case Number.isNaN(Number(age)):
+//     console.log("You input was not number.");
+//     break;
+//   case age >= 18:
+//     console.log("You are old enough to drive.");
+//     break;
+//   default:
+//     console.log(`You are left with ${18 - age} years to drive.`);
+// }
+
+// let yourAge = prompt("Enter your age:");
+
+// switch (true) {
+//   case Number.isNaN(Number(yourAge)):
+//     console.log("You input was not number.");
+//     break;
+//   case yourAge > 41:
+//     console.log(`You are ${yourAge - 41} years older than me.`);
+//     break;
+//   case yourAge == 41:
+//     console.log("You are same ages to me.");
+//     break;
+//   case yourAge < 41:
+//     console.log(`You are ${41 - yourAge} years younger than me.`);
+//     break;
+// }
+
+let a = 4;
+let b = 3;
+
+if (a > b) {
+  console.log(`${a} is greater than ${b}`);
+} else {
+  console.log(`${a} is less than ${b}`);
 }
 
-let yourAge = prompt("Enter your age:");
-
-switch (true) {
-  case Number.isNaN(Number(yourAge)):
-    console.log("You input was not number.");
-    break;
-  case yourAge > 41:
-    console.log(`You are ${yourAge - 41} years older than me.`);
-    break;
-  case yourAge == 41:
-    console.log("You are same ages to me.");
-    break;
-  case yourAge < 41:
-    console.log(`You are ${41 - yourAge} years younger than me.`);
-    break;
-}
+a > b
+  ? console.log(`${a} is greater than ${b}`)
+  : console.log(`${a} is less than ${b}`);
