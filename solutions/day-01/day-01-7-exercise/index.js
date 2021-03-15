008aebec0d85f1a2de3456fd014e44bcdb03edc2
nobu09 <nobu0922@gmail.com>
2021-03-15 09:15:42 +0900

    Day1 7 Object Exercise level1 end

--- solutions/day-01/day-01-7-exercise/index.js
+++ solutions/day-01/day-01-7-exercise/index.js
@@ -1,3 +1,26 @@
 // Exercise level1
 const dog = {};
 console.log(dog);
+
+dog.name = "Iro";
+dog.legs = 2;
+dog.color = "white";
+dog.age = 5;
+dog.bark = function () {
+  return "woof woof";
+};
+
+console.log(dog);
+console.log(dog.name);
+console.log(dog.legs);
+console.log(dog.color);
+console.log(dog.age);
+console.log(dog.bark());
+
+dog.breed = "Italian Greyhound";
+dog.getDogInfo = function () {
+  return `name: ${this.name}, legs: ${this.legs}, color: ${this.color}, age: ${
+    this.age
+  }, bark: ${this.bark()}, breed: ${this.breed}`;
+};
+console.log(dog.getDogInfo());
