ce44e0c320b036dbc1d9ee88ee733b880646a82e
nobu09 <nobu0922@gmail.com>
2021-03-18 09:02:37 +0900

    Day1 7 Object Exercise level2

--- solutions/day-01/day-01-7-exercise/index.js
+++ solutions/day-01/day-01-7-exercise/index.js
@@ -24,3 +24,83 @@ dog.getDogInfo = function () {
   }, bark: ${this.bark()}, breed: ${this.breed}`;
 };
 console.log(dog.getDogInfo());
+
+const users = {
+  Alex: {
+    email: "alex@alex.com",
+    skills: ["HTML", "CSS", "JavaScript"],
+    age: 20,
+    isLoggedIn: false,
+    points: 30,
+  },
+  Asab: {
+    email: "asab@asab.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Redux",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 25,
+    isLoggedIn: false,
+    points: 50,
+  },
+  Brook: {
+    email: "daniel@daniel.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
+    age: 30,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Daniel: {
+    email: "daniel@alex.com",
+    skills: ["HTML", "CSS", "JavaScript", "Python"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  John: {
+    email: "john@john.com",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Redux", "Node.js"],
+    age: 20,
+    isLoggedIn: true,
+    points: 50,
+  },
+  Thomas: {
+    email: "thomas@thomas.com",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+  Paul: {
+    email: "paul@paul.com",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "MongoDB",
+      "Express",
+      "React",
+      "Node",
+    ],
+    age: 20,
+    isLoggedIn: false,
+    points: 40,
+  },
+};
+
+const usersArray = Object.entries(users);
+const skillful_user = usersArray.reduce((skillful_user, user) => {
+  if (skillful_user[1].skills.length < user[1].skills.length) {
+    skillful_user = user;
+  }
+
+  return skillful_user;
+}, usersArray[0]);
+
+console.log(skillful_user);
